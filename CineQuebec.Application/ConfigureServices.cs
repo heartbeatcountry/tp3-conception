8be2c8cac0a7e7a69b9e6526f17331032ee7c6de
pbreton <13599639-pbreton@users.noreply.gitlab.com>
2024-04-05 21:20:17 -0400

    injecter les dépendences dans le wpf

---
+++ CineQuebec.Application/ConfigureServices.cs
@@ -0,0 +1,15 @@
+using CineQuebec.Application.Interfaces.Services;
+using CineQuebec.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CineQuebec.Application;
+
+public static class ConfigureServices
+{
+	public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+	{
+		return services
+			.AddSingleton<IFilmQueryService, FilmQueryService>()
+			.AddSingleton<IFilmCreationService, FilmCreationService>();
+	}
+}
--- CineQuebec.Windows/App.xaml.cs
+++ CineQuebec.Windows/App.xaml.cs
@@ -1,10 +1,44 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Windows;
+using CineQuebec.Application;
+using CineQuebec.Persistence;
+using CineQuebec.Windows.View;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace CineQuebec.Windows;
 
 /// <summary>
 ///     Interaction logic for App.xaml
 /// </summary>
-public partial class App : Application
+public partial class App : System.Windows.Application
 {
+    internal static IServiceProvider ServiceProvider { get; set; } = default!;
+
+    protected override void OnStartup (StartupEventArgs e)
+    {
+        var configuration = GetConfiguration();
+        var serviceCollection = ConfigureServices(configuration);
+
+        ServiceProvider = serviceCollection.BuildServiceProvider();
+
+        var mainWindow = ServiceProvider.GetRequiredService<MainWindow>();
+        mainWindow.Show();
+    }
+
+    private static IConfiguration GetConfiguration()
+    {
+        return new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.local.json", optional: true, reloadOnChange: true)
+            .Build();
+    }
+
+    private static IServiceCollection ConfigureServices (IConfiguration configuration)
+    {
+        return new ServiceCollection()
+            .AddPersistenceServices(configuration)
+            .AddApplicationServices()
+            .AddTransient(typeof(MainWindow));
+    }
 }
