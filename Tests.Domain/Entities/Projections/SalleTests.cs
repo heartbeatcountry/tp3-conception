720961671cb640e05ce72cfb7a22655f2ba83e41
Cynthia Fortier <cyfor001@edu.cegepgarneau.com>
2024-04-09 13:09:05 -0400

    création des fichiers pour les tests

---
+++ Tests.Domain/Entities/Projections/SalleTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Domain.Entities.Projections
+{
+    internal class SalleTests
+    {
+    }
+}
