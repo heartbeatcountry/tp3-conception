8118232a660c5a47cf56c2232796bb0879aad50b
Cynthia Fortier <cyfor001@edu.cegepgarneau.com>
2024-04-23 13:47:05 -0400

    Entité, interface et test pour NoteFilm.

---
+++ CineQuebec.Domain/Interfaces/Entities/Films/INoteFilm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineQuebec.Domain.Interfaces.Entities.Films
+{
+    public interface INoteFilm : IEntite
+    {
+
+        Guid IdUtilisateur { get; }
+        Guid IdFilm{ get; }
+        ushort Note {  get; set; }
+        void SetUtilisateur(Guid pIdUtilisateur);
+        void SetFilm(Guid pIdFilm);
+        void SetNoteFilm(ushort pNoteObtenue);
+
+
+
+    }
+}
