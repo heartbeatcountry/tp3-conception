c0c5023e958e20cb8c68fe3771cd50b921150be1
pbreton <13599639-pbreton@users.noreply.gitlab.com>
2024-05-11 21:30:39 -0400

    ajouter la fonctionnalité de récompenses

---
+++ CineQuebec.Windows/Interfaces/ViewModels/Screens/Admin/IAdminOffrirBilletsViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Controls;
+
+using CineQuebec.Application.Records.Films;
+using CineQuebec.Application.Records.Identity;
+using CineQuebec.Application.Records.Projections;
+using CineQuebec.Windows.Interfaces.ViewModels.Components;
+using CineQuebec.Windows.Records;
+
+using Stylet;
+
+namespace CineQuebec.Windows.Interfaces.ViewModels.Screens.Admin;
+
+public interface IAdminOffrirBilletsViewModel : IScreen
+{
+    IHeaderViewModel HeaderViewModel { get; }
+    BindableCollection<FilmDto> LstFilms { get; }
+    BindableCollection<ProjectionDto> LstProjections { get; }
+    BindableCollection<SelectedItemWrapper<UtilisateurDto>> LstUtilisateurs { get; }
+    FilmDto? FilmSelectionne { get; set; }
+    ProjectionDto? ProjectionSelectionnee { get; set; }
+    bool CanOffrirBillets { get; }
+    void OnUtilisateurSelectionChange(SelectionChangedEventArgs evt);
+    Task RafraichirFilms();
+    Task OffrirBillets();
+}
