86a6ca7b495c83da5a678cb864a0cfb8308d7144
Cynthia Fortier <cyfor001@edu.cegepgarneau.com>
2024-05-12 17:26:32 -0400

    Affichage de la boite dialogue pour noter un film.

---
+++ CineQuebec.Windows/Interfaces/ViewModels/Dialogs/IDialogNoterFilmViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Stylet;
+
+namespace CineQuebec.Windows.Interfaces.ViewModels.Dialogs;
+
+public interface IDialogNoterFilmViewModel : IScreen
+{
+    bool AValide { get; }
+    byte NoteFilm{ get; set; }
+    void Annuler();
+    void Valider();
+}
